c1e478e7d5e3598d5642e606f5905778c5eb6fd6
Roksana <roksana.swierczek96@gmail.com>
2019-05-22 20:45:47 +0200

    added autocompleter

---
+++ autocompleter.js
@@ -0,0 +1,39 @@
+Vue.component('v-autocompleter', {
+    template: `
+        <div class="v-autocompleter">
+            <input class="v-autocompleter-input" type="text" v-model="input">
+				<ul v-if="input.length > 0">
+					
+                    <li v-for="animal in data">
+						{{ animal }}
+                    </li>
+					
+           
+            </ul>
+            </div>
+    `
+	,
+	props: {
+        data: { type: Array, required: true },
+    }
+    ,
+    computed:{
+        data: function () {			
+            var self = this;
+            var filteredAnimals = animals.filter( function(animal) {
+                return animal.toLowerCase().startsWith(self.input.toLowerCase());
+            });
+            return filteredAnimals.length > 9 ? filteredAnimals.slice(0, 10) : filteredAnimals;
+        }  
+    },
+    data() {
+        return {
+            input: ''
+        }
+    }
+})
+
+new Vue ({
+    el: ".mainSearch",
+	
+})
